5cb6a2e6a5047710edae76ad506611d9ffd6d80e
TheLegendaryTea <91951734+TheLegendaryTea@users.noreply.github.com>
2021-12-23 18:59:39 +0200

    Update Program.cs

--- ConsoleApp34/Program.cs
+++ ConsoleApp34/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+using System;
 
-namespace ConsoleApp34
+namespace ConsoleApp32
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = 5;
+            int[,] matr = new int[n, n];
+            int[] array = new int[n];
+            Random r = new Random();
+            Console.WriteLine("Исходная матрица:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matr[i, j] = r.Next(-10, 11);
+                    Console.Write(matr[i, j].ToString() + "\t");
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Массив из максимальных элементов в каждом столбце:");
+            for (int j = 0; j < n; j++)
+            {
+                int max = int.MinValue;
+                for (int i = 0; i < n; i++)
+                {
+                    if (matr[i, j] > max)
+                    {
+                        max = matr[i, j];
+                    }
+                }
+                array[j] = max;
+                Console.Write(array[j].ToString() + "\t");
+            }
+            Console.WriteLine();
         }
     }
 }
